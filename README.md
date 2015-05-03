78fd2aecc34b732abb23221518ba7c8f9f61a804
Elliott Friedman <elliott.friedman@gmail.com>
2015-05-03 14:37:18 -0700

    fix readme and path issues

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ I wrote this to be alerted on new colors and styles because these are mostly all
 It requires that casperjs be installed from nodejs and bsd-mailx for sending the multipart emails (html).
 
 Modify the shell script to match your environment and run it via cron.
+
+##Sample Email
+New:
+
+[![New](http://www.eyyit.com/jcrew/jcrew_polos_cur.png)](http://www.eyyit.com/jcrew/jcrew_polos_cur.png)
+
+Old:
+
+[![Old](http://www.eyyit.com/jcrew/jcrew_polos_old.png)](http://www.eyyit.com/jcrew/jcrew_polos_old.png)
--- jcrew_polo_tracker.sh
+++ jcrew_polo_tracker.sh
@@ -10,30 +10,26 @@ WWW_DIR="/var/www/www.eyyit.com/www/jcrew"
 JCREW_URL='http://www.jcrew.com/mens_category/polostees/shortsleevepolos/PRDOVR~91918/91918.jsp'
 URL='http://www.eyyit.com/jcrew'
 
-OLD_PREFIX='jcrew_polos_old'
-CUR_PREFIX='jcrew_polos_cur'
+CUR_FILE='jcrew_polos_cur.png'
+NEW_FILE='jcrew_polos_new.png'
+OLD_FILE='jcrew_polos_old.png'
 
 SCRAPER="${LOCAL_DIR}/jcrew_polo_tracker_scraper.js"
-NEW_FILE="${WWW_DIR}/jcrew_polos.png"
-OLD_FILE=$(find ${WWW_DIR} -type f -name "${OLD_PREFIX}*.png" | grep 'png' || \
-  (touch ${WWW_DIR}/${OLD_PREFIX}.0.png && echo ${WWW_DIR}/${OLD_PREFIX}.0.png))
-CUR_FILE=$(find ${WWW_DIR} -type f -name "${CUR_PREFIX}*.png" | grep 'png' || \
-  (touch ${WWW_DIR}/${CUR_PREFIX}.0.png && echo ${WWW_DIR}/${CUR_PREFIX}.0.png))
 
 timeout -s KILL 5m \
   casperjs ${SCRAPER} \
-    --filename="${NEW_FILE}" \
+    --filename="${WWW_DIR}/${NEW_FILE}" \
     --url="${JCREW_URL}" || exit
 
-new_sha1=$(sha1sum ${NEW_FILE} | cut -d ' ' -f 1)
-cur_sha1=$(basename ${CUR_FILE} | cut -d '.' -f 3)
+cur_sha1=$(sha1sum ${WWW_DIR}/${CUR_FILE} | cut -d ' ' -f 1)
+new_sha1=$(sha1sum ${WWW_DIR}/${NEW_FILE} | cut -d ' ' -f 1)
 
-if [[ "${new_sha1}" == "${cur_sha1}" ]]; then
-  rm -f ${NEW_FILE}
+if [[ "${new_sha1}" = "${cur_sha1}" ]]; then
+  rm -f ${WWW_DIR}/${NEW_FILE}
 else
-  rm -f ${OLD_FILE}
-  mv -f ${CUR_FILE} ${WWW_DIR}/${OLD_PREFIX}.${cur_sha1}.png
-  mv -f ${NEW_FILE} ${WWW_DIR}/${CUR_PREFIX}.${new_sha1}.png
+  rm -f ${WWW_DIR}/${OLD_FILE}
+  mv -f ${WWW_DIR}/${CUR_FILE} ${WWW_DIR}/${OLD_FILE}
+  mv -f ${WWW_DIR}/${NEW_FILE} ${WWW_DIR}/${CUR_FILE}
   (cat << EOF
 --001a11c133c8af0b9a04e750283e
 Content-Type: text/plain; charset=ISO-8859-1
@@ -49,14 +45,14 @@ Content-Type: text/html; charset=ISO-8859-1
   <b>New:</b>
   <blockquote style="margin:0 0 0 40px;border:none;padding:0px">
     <a href="${JCREW_URL}">
-      <img src="${URL}/${CUR_PREFIX}.${new_sha1}.png">
+      <img src="${URL}/${CUR_FILE}">
     </a>
   </blockquote>
   <br><br>
   <b>Old:</b>
   <blockquote style="margin:0 0 0 40px;border:none;padding:0px">
     <a href="${JCREW_URL}">
-      <img src="${URL}/${OLD_PREFIX}.${cur_sha1}.png">
+      <img src="${URL}/${OLD_FILE}">
     </a>
   </blockquote>
 

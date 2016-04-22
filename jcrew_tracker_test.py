335e5ec1991f407e4ef2d47a62325cafecd7763f
Elliott Friedman <elliott.friedman@gmail.com>
2016-04-22 01:07:58 -0700

    update tests

--- jcrew_tracker_test.py
+++ jcrew_tracker_test.py
@@ -1,45 +1,45 @@
 #!/usr/bin/env python
 
-from jcrew_tracker import JCrewTracker
+import jcrew_tracker
 import unittest
 
 class Test(unittest.TestCase):
   @classmethod
   def setUp(self):
-    self.jt = JCrewTracker()
+    self.jt = jcrew_tracker
 
-  def test_GetChanges_NewItems(self):
+  def test_get_changes_NewItems(self):
     colors = {'a': {'active': True, 'price': 5, 'name': 'a'},
               'b': {'active': True, 'price': 5, 'name': 'b'},
               'c': {'active': True, 'price': 5, 'name': 'c'}}
     state = {'a': {'active': True, 'price': 5, 'name': 'a'},
              'b': {'active': False, 'price': 5, 'name': 'b'}}
-    changes = self.jt.GetChanges(colors, state)
+    changes = self.jt.get_changes(colors, state)
     self.assertDictEqual(
       {'New Items': ['b', 'c'], 'Price Changes': [], 'Removed Items': []},
       changes)
 
-  def test_GetChanges_RemovedItems(self):
+  def test_get_changes_RemovedItems(self):
     colors = {'a': {'active': True, 'price': 5, 'name': 'a'}}
     state = {'a': {'active': True, 'price': 5, 'name': 'a'},
              'b': {'active': True, 'price': 5, 'name': 'b'},
              'c': {'active': False, 'price': 5, 'name': 'c'}}
-    changes = self.jt.GetChanges(colors, state)
+    changes = self.jt.get_changes(colors, state)
     self.assertDictEqual(
       {'New Items': [], 'Price Changes': [], 'Removed Items': ['b']},
       changes)
 
-  def test_GetChanges_PriceChangedItems(self):
+  def test_get_changes_PriceChangedItems(self):
     colors = {'a': {'active': True, 'price': 5, 'name': 'a'},
               'b': {'active': True, 'price': 0, 'name': 'b'}}
     state = {'a': {'active': True, 'price': 0, 'name': 'a'},
              'b': {'active': True, 'price': 5, 'name': 'b'}}
-    changes = self.jt.GetChanges(colors, state)
+    changes = self.jt.get_changes(colors, state)
     self.assertDictEqual(
       {'New Items': [], 'Price Changes': ['a', 'b'], 'Removed Items': []},
       changes)
 
-  def test_GetChanges_AllChanges(self):
+  def test_get_changes_AllChanges(self):
     colors = {'a': {'active': True, 'price': 5, 'name': 'a'},
               'b': {'active': True, 'price': 0, 'name': 'b'},
               'c': {'active': True, 'price': 0, 'name': 'c'},
@@ -48,7 +48,7 @@ class Test(unittest.TestCase):
              'b': {'active': True, 'price': 5, 'name': 'b'},
              'c': {'active': False, 'price': 0, 'name': 'c'},
              'e': {'active': True, 'price': 5, 'name': 'e'}}
-    changes = self.jt.GetChanges(colors, state)
+    changes = self.jt.get_changes(colors, state)
     self.assertDictEqual(
       {'New Items': ['c', 'd'], 'Price Changes': ['a', 'b'],
        'Removed Items': ['e']}, changes)
